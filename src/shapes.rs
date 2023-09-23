6c4d528c143e614c38793333dae1d96467eb8705
Cameron <cameron.lyons2@gmail.com>
2023-09-23 13:41:21 -0500

    ray intersect

--- src/shapes.rs
+++ src/shapes.rs
@@ -24,3 +24,23 @@ impl Sphere {
     fn new(center: Vec3f, radius: f32) -> Sphere {
         Sphere { center, radius }
     }
+
+    fn ray_intersect(&self, orig: &Vec3f, dir: &Vec3f) -> Option<f32> {
+        let l = self.center.subtract(orig);
+        let tca = l.dot(dir);
+        let d2 = l.magnitude_squared() - tca * tca;
+        if d2 > self.radius * self.radius {
+            return None;
+        }
+        let thc = (self.radius * self.radius - d2).sqrt();
+        let mut t0 = tca - thc;
+        let t1 = tca + thc;
+        if t0 < 0.0 {
+            t0 = t1;
+        }
+        if t0 < 0.0 {
+            return None;
+        }
+        Some(t0)
+    }
+}
