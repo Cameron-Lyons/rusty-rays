e0e748a9e1c4cc87a3c9099702339fd0c8e8280d
Cameron <cameron.lyons2@gmail.com>
2023-09-23 13:14:48 -0500

    write to file

--- render.rs
+++ render.rs
@@ -4,11 +4,25 @@ use std::io::prelude::*;
 type Vec3f = (f32, f32, f32);
 
 fn render(width: usize, height: usize) {
-
-    let mut framebuffer: Vec<Vec3f> = vec![(0.0, 0.0, 0.0); WIDTH * HEIGHT];
+    let mut framebuffer: Vec<Vec3f> = vec![(0.0, 0.0, 0.0); width * height];
 
     for j in 0..height {
         for i in 0..width {
-            framebuffer[i + j * WIDTH] = (j as f32 / HEIGHT as f32, i as f32 / WIDTH as f32, 0.0);
+            framebuffer[i + j * width] = (j as f32 / height as f32, i as f32 / width as f32, 0.0);
         }
     }
+
+    let mut ofs = File::create("./out.ppm").expect("Unable to create file");
+    ofs.write_all(format!("P6\n{} {}\n255\n", WIDTH, HEIGHT).as_bytes())
+        .expect("Failed to write to file");
+
+    for &(r, g, b) in &framebuffer {
+        let max_value = 255.0;
+        ofs.write_all(&[
+            (max_value * r.clamp(0.0, 1.0)) as u8,
+            (max_value * g.clamp(0.0, 1.0)) as u8,
+            (max_value * b.clamp(0.0, 1.0)) as u8,
+        ])
+        .expect("Failed to write to file");
+    }
+}
