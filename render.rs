81363ec57cced79b8ebf815ded358b34d223706a
Cameron <cameron.lyons2@gmail.com>
2023-09-23 13:13:42 -0500

    args

--- render.rs
+++ render.rs
@@ -3,14 +3,12 @@ use std::io::prelude::*;
 
 type Vec3f = (f32, f32, f32);
 
-fn render() {
-    const WIDTH: usize = 1024;
-    const HEIGHT: usize = 768;
+fn render(width: usize, height: usize) {
 
     let mut framebuffer: Vec<Vec3f> = vec![(0.0, 0.0, 0.0); WIDTH * HEIGHT];
 
-    for j in 0..HEIGHT {
-        for i in 0..WIDTH {
+    for j in 0..height {
+        for i in 0..width {
             framebuffer[i + j * WIDTH] = (j as f32 / HEIGHT as f32, i as f32 / WIDTH as f32, 0.0);
         }
     }
