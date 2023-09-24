9bf6b52a16517b19e0015a302c8a6dc1b8e5f6c2
Cameron <cameron.lyons2@gmail.com>
2023-09-24 08:25:45 -0500

    ivory

--- material.rs
+++ material.rs
@@ -5,3 +5,10 @@ struct Material {
     diffuse_color: Vec3,
     specular_exponent: f32,
 }
+
+const IVORY: Material = Material {
+    refractive_index: 1.0,
+    albedo: [0.9, 0.5, 0.1, 0.0],
+    diffuse_color: Vec3(0.4, 0.4, 0.3),
+    specular_exponent: 50.0,
+};
