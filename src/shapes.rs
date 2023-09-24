c09e1c533ff8f5bc5101355f9d07831a00f962cb
Cameron <cameron.lyons2@gmail.com>
2023-09-24 09:27:09 -0500

    vec3

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,20 +1,3 @@
-#[derive(Clone, Copy, Debug)]
-struct Vec3f(f32, f32, f32);
-
-impl Vec3f {
-    fn subtract(&self, other: &Vec3f) -> Vec3f {
-        Vec3f(self.0 - other.0, self.1 - other.1, self.2 - other.2)
-    }
-
-    fn dot(&self, other: &Vec3f) -> f32 {
-        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
-    }
-
-    fn magnitude_squared(&self) -> f32 {
-        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
-    }
-}
-
 struct Sphere {
     center: Vec3f,
     radius: f32,
