4b74d1a2afee444186a2bbffa258d677c036835b
Cameron <cameron.lyons2@gmail.com>
2023-09-24 14:46:21 -0500

    vec3

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,15 @@ fn render(width: usize, height: usize) -> io::Result<()> {
 
     for j in 0..height {
         for i in 0..width {
-            framebuffer[i + j * width] = (j as f32 / height as f32, i as f32 / width as f32, 0.0);
+            framebuffer[i + j * width] =
+                vec3::Vec3f(j as f32 / height as f32, i as f32 / width as f32, 0.0);
         }
     }
 
     let mut ofs = File::create("./out.ppm")?;
     ofs.write_all(format!("P6\n{} {}\n255\n", width, height).as_bytes())?;
 
-    for &(r, g, b) in &framebuffer {
+    for &vec3::Vec3f(r, g, b) in &framebuffer {
         let max_value = 255.0;
         ofs.write_all(&[
             (max_value * r.clamp(0.0, 1.0)) as u8,
