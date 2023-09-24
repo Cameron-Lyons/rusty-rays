5a155feb1b227efa2094396bb5bcec65a0ecf4e3
Cameron <cameron.lyons2@gmail.com>
2023-09-24 18:59:48 -0500

    reflect

--- src/light.rs
+++ src/light.rs
@@ -1,6 +1,8 @@
-#[derive(Clone, Copy, Debug)]
-pub struct Light {
-    position: Vec3f,
-    color: Vec3f, // Using Vec3f for RGB color
-    intensity: f32,
+pub struct Lights {
+    pub sources: [Vec3f; 3],
 }
+
+impl Lights {
+    pub fn reflect(&self, I: &Vec3f, N: &Vec3f) -> Vec3f {
+        I.subtract(&N.multiply_scalar(2.0 * I.dot(N)))
+    }
