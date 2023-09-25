a2e53b405478a59feda51e62b5feef52c4ba70e1
Cameron <cameron.lyons2@gmail.com>
2023-09-24 19:09:39 -0500

    ray

--- src/shapes.rs
+++ src/shapes.rs
@@ -29,3 +29,36 @@ impl Sphere {
         Some(t0)
     }
 }
+
+fn cast_ray(orig: &Vec3f, dir: &Vec3f, depth: i32) -> Vec3f {
+    let (hit, point, n, material) = scene_intersect(orig, dir);
+    if depth > 4 || !hit {
+        return Vec3f(0.2, 0.7, 0.8); // background color
+    }
+
+    let reflect_dir = reflect(dir, &n).normalized();
+    let refract_dir = refract(dir, &n, material.refractive_index).normalized();
+    let reflect_color = cast_ray(&point, &reflect_dir, depth + 1);
+    let refract_color = cast_ray(&point, &refract_dir, depth + 1);
+
+    let mut diffuse_light_intensity = 0.0;
+    let mut specular_light_intensity = 0.0;
+    for light in &LIGHTS {
+        let light_dir = light.subtract(&point).normalized();
+        let (shadow_hit, shadow_pt, _, _) = scene_intersect(&point, &light_dir);
+        if shadow_hit && (shadow_pt.subtract(&point).norm() < light.subtract(&point).norm()) {
+            continue;
+        }
+        diffuse_light_intensity += f32::max(0.0, light_dir.dot(&n));
+        specular_light_intensity += f32::powf(
+            f32::max(0.0, -reflect(&light_dir.negate(), &n).dot(dir)),
+            material.specular_exponent,
+        );
+    }
+    material
+        .diffuse_color
+        .multiply_scalar(diffuse_light_intensity * material.albedo[0])
+        .add(&Vec3f(1.0, 1.0, 1.0).multiply_scalar(specular_light_intensity * material.albedo[1]))
+        .add(&reflect_color.multiply_scalar(material.albedo[2]))
+        .add(&refract_color.multiply_scalar(material.albedo[3]))
+}
