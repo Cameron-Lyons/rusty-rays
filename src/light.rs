829529390fe29e94bea1ffd862d81a1746fab114
Cameron <cameron.lyons2@gmail.com>
2023-09-24 19:02:04 -0500

    negate

--- src/light.rs
+++ src/light.rs
@@ -6,3 +6,19 @@ impl Lights {
     pub fn reflect(&self, I: &Vec3f, N: &Vec3f) -> Vec3f {
         I.subtract(&N.multiply_scalar(2.0 * I.dot(N)))
     }
+
+    pub fn refract(&self, I: &Vec3f, N: &Vec3f, eta_t: f32, eta_i: f32) -> Vec3f {
+        let cosi = -f32::max(-1.0, f32::min(1.0, I.dot(N)));
+        if cosi < 0.0 {
+            return self.refract(I, &N.negate(), eta_i, eta_t);
+        }
+        let eta = eta_i / eta_t;
+        let k = 1.0 - eta * eta * (1.0 - cosi * cosi);
+        if k < 0.0 {
+            Vec3f(1.0, 0.0, 0.0)
+        } else {
+            I.multiply_scalar(eta)
+                .add(&N.multiply_scalar(eta * cosi - k.sqrt()))
+        }
+    }
+}
--- src/vec3.rs
+++ src/vec3.rs
@@ -2,15 +2,23 @@
 pub struct Vec3f(pub f32, pub f32, pub f32);
 
 impl Vec3f {
-    fn subtract(&self, other: &Vec3f) -> Vec3f {
+    pub fn subtract(&self, other: &Self) -> Self {
         Vec3f(self.0 - other.0, self.1 - other.1, self.2 - other.2)
     }
 
-    fn dot(&self, other: &Vec3f) -> f32 {
-        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
+    pub fn add(&self, other: &Self) -> Self {
+        Vec3f(self.0 + other.0, self.1 + other.1, self.2 + other.2)
     }
 
-    fn magnitude_squared(&self) -> f32 {
-        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+    pub fn multiply_scalar(&self, scalar: f32) -> Self {
+        Vec3f(self.0 * scalar, self.1 * scalar, self.2 * scalar)
+    }
+
+    pub fn negate(&self) -> Self {
+        Vec3f(-self.0, -self.1, -self.2)
+    }
+
+    pub fn dot(&self, other: &Self) -> f32 {
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
     }
 }
