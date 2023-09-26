a83e56971be09f8186a7e025c2715997d6faf91d
Cameron <cameron.lyons2@gmail.com>
2023-09-25 21:57:19 -0500

    box

--- src/shapes.rs
+++ src/shapes.rs
@@ -29,3 +29,33 @@ impl Sphere {
         Some(t0)
     }
 }
+
+pub struct Box {
+    min: Vec3f,
+    max: Vec3f,
+}
+
+impl Box {
+    pub fn new(min: Vec3f, max: Vec3f) -> Rectangle {
+        Rectangle { min, max }
+    }
+
+    pub fn ray_intersect(&self, orig: &Vec3f, dir: &Vec3f) -> Option<f32> {
+        let t1 = (self.min.0 - orig.0) / dir.0;
+        let t2 = (self.max.0 - orig.0) / dir.0;
+        let t3 = (self.min.1 - orig.1) / dir.1;
+        let t4 = (self.max.1 - orig.1) / dir.1;
+        let t5 = (self.min.2 - orig.2) / dir.2;
+        let t6 = (self.max.2 - orig.2) / dir.2;
+
+        let tmin = t1.min(t2).max(t3.min(t4)).max(t5.min(t6));
+        let tmax = t1.max(t2).min(t3.max(t4)).min(t5.max(t6));
+
+        if tmax < 0.0 || tmin > tmax {
+            return None;
+        }
+
+        let t = if tmin < 0.0 { tmax } else { tmin };
+        Some(t)
+    }
+}
