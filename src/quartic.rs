8e6a8fae3229173b1ffe1dfcad31e28950104b36
Cameron Lyons <cameron.lyons2@gmail.com>
2023-10-14 10:14:43 -0500

    cubic

--- src/quartic.rs
+++ src/quartic.rs
@@ -50,3 +50,18 @@ fn solve_quartic(coeffs: &[f32; 5]) -> Vec<f32> {
 
     roots
 }
+
+fn solve_cubic(coeffs: &[f32; 4]) -> Vec<f32> {
+    let a = coeffs[0];
+    let b = coeffs[1] / a;
+    let c = coeffs[2] / a;
+    let d = coeffs[3] / a;
+
+    let delta_0 = c * c - 3.0 * b * d + 12.0 * a * e;
+    let delta_1 = 2.0 * c * c * c - 9.0 * b * c * d + 27.0 * a * d * d + 27.0 * b * b * e - 72.0 * a * c * e;
+    
+    let c = ((delta_1 + (delta_1 * delta_1 - 4.0 * delta_0 * delta_0 * delta_0).sqrt()).powf(1.0/3.0)) / (3.0f32.cbrt() * 2.0f32.powf(1.0/3.0));
+    let roots = vec![(-1.0/(3.0*a))*(b + c + delta_0/c)];
+
+    roots
+}
