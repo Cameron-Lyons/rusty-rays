4e591fb6a531e45e7def551b510864d817b511b2
Cameron Lyons <cameron.lyons2@gmail.com>
2023-10-17 18:16:57 -0500

    error handle

--- src/quartic.rs
+++ src/quartic.rs
@@ -1,11 +1,16 @@
 const EPSILON: f32 = 1e-12;
 
-fn solve_quartic(coeffs: &[f32; 5]) -> Vec<f32> {
-    let a = coeffs[0];
-    let b = coeffs[1] / a;
-    let c = coeffs[2] / a;
-    let d = coeffs[3] / a;
-    let e = coeffs[4] / a;
+fn solve_quartic(coeffs: &[f32; 5], tolerance: f32 = 1e-12) -> Vec<f32> {
+    let [a, b, c, d, e] = *coeffs;
+    
+    if a.abs() < tolerance {
+        panic!("The leading coefficient must not be zero.");
+    }
+
+    let b = b / a;
+    let c = c / a;
+    let d = d / a;
+    let e = e / a;
 
     let sq = b * b;
 
@@ -13,8 +18,9 @@ fn solve_quartic(coeffs: &[f32; 5]) -> Vec<f32> {
     let q = 1.0 / 8.0 * sq * b - 0.5 * b * c + d;
     let r = -3.0 / 256.0 * sq * sq + c * sq / 16.0 - 1.0 / 4.0 * b * d + e;
 
-    if r.abs() < 1e-12 {
-        return solve_cubic(&[a, c, d, e]);
+    // Degenerate case: quartic reduces to cubic
+    if r.abs() < tolerance {
+        return solve_cubic(&[1.0, b, c, d]);
     }
 
     let cubic_coeffs = [
@@ -27,8 +33,8 @@ fn solve_quartic(coeffs: &[f32; 5]) -> Vec<f32> {
     let z = solve_cubic(&cubic_coeffs).into_iter().next().unwrap_or(0.0);
 
     let d1 = 2.0 * z - p;
-    let d2 = if d1.abs() < 1e-12 {
-        -q / sqrt(2.0 * z)
+    let d2 = if d1.abs() < tolerance {
+        -q / (2.0 * z).sqrt()
     } else {
         q / (2.0 * z)
     };
