b49b8eeca195ffc50851857708bc2a36cc689a68
Cameron Lyons <cameron.lyons2@gmail.com>
2023-10-24 23:22:39 -0500

    main

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,28 @@
 use std::fs::File;
-use std::io::{self, prelude::*};
-use vec3::Vec3f;
+use std::io::{self, Write};
+use std::path::Path;
 
 mod vec3;
+use vec3::Vec3f;
 
 fn create_gradient_image(width: usize, height: usize) -> Vec<Vec3f> {
-    let mut framebuffer: Vec<Vec3f> = vec![Vec3f(0.0, 0.0, 0.0); width * height];
-
+    let mut framebuffer = Vec::with_capacity(width * height);
     for j in 0..height {
         for i in 0..width {
-            framebuffer[i + j * width] =
-                Vec3f(j as f32 / height as f32, i as f32 / width as f32, 0.0);
+            framebuffer.push(Vec3f(j as f32 / height as f32, i as f32 / width as f32, 0.0));
         }
     }
-
     framebuffer
 }
 
-fn render(width: usize, height: usize) -> io::Result<()> {
-    let mut framebuffer: Vec<vec3::Vec3f> = vec![vec3::Vec3f(0.0, 0.0, 0.0); width * height];
+fn render(width: usize, height: usize, path: &Path) -> io::Result<()> {
+    let framebuffer = create_gradient_image(width, height);
+    let mut file = File::create(path)?;
 
-    for j in 0..height {
-        for i in 0..width {
-            framebuffer[i + j * width] =
-                vec3::Vec3f(j as f32 / height as f32, i as f32 / width as f32, 0.0);
-        }
-    }
-
-    let mut ofs = File::create("./out.ppm")?;
-    ofs.write_all(format!("P6\n{} {}\n255\n", width, height).as_bytes())?;
-
-    for &vec3::Vec3f(r, g, b) in &framebuffer {
+    writeln!(file, "P6\n{} {}\n255", width, height)?;
+    for Vec3f(r, g, b) in framebuffer {
         let max_value = 255.0;
-        ofs.write_all(&[
+        file.write_all(&[
             (max_value * r.clamp(0.0, 1.0)) as u8,
             (max_value * g.clamp(0.0, 1.0)) as u8,
             (max_value * b.clamp(0.0, 1.0)) as u8,
@@ -42,8 +32,8 @@ fn render(width: usize, height: usize) -> io::Result<()> {
     Ok(())
 }
 
-fn main() {
-    if let Err(e) = render(1024, 768) {
-        eprintln!("Error: {}", e);
-    }
+fn main() -> Result<(), io::Error> {
+    render(1024, 768, Path::new("out.ppm"))?;
+    Ok(())
 }
+
